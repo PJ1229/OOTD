2ae251eb1f220871544f05888f383f3286e97d9c
Kieran Llarena <baymax709@gmail.com>
2025-02-01 13:33:19 -0500

    chore: next-pwa next.config.ts config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public", // Directory where service worker and assets will be stored
+  register: true, // Automatically register the service worker
+  skipWaiting: true, // Activate new service worker immediately
+});
+
+const finalConfig: NextConfig = {
+  ...nextConfig,
+  ...pwaConfig,
+  reactStrictMode: true,
+};
+
+export default finalConfig;
