a8f3f3858f3bfb1485a9bec33bbfc6f1940b23b7
irwin patcharaput <irwinp@umich.edu>
2025-02-01 16:23:05 -0500

    added a swipe page, but i didnt rename the files so if we merge it ggs.

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
-import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+// next.config.js
+const withPWA = require('next-pwa');
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-const pwaConfig = withPWA({
-  dest: "public", // Directory where service worker and assets will be stored
-  register: true, // Automatically register the service worker
-  skipWaiting: true, // Activate new service worker immediately
+module.exports = withPWA({
+  dest: 'public',          // Directory where service worker and assets will be stored
+  register: true,          // Automatically register the service worker
+  skipWaiting: true,       // Activate new service worker immediately
+  reactStrictMode: true,   // Enable React strict mode
 });
-
-const finalConfig: NextConfig = {
-  ...nextConfig,
-  ...pwaConfig,
-  reactStrictMode: true,
-};
-
-export default finalConfig;
